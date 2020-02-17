8309c18de7516fb564bc677e8d8dc7f441842d84
Praya Amadiga Pitasa <prayaamadigapitasa@gmail.com>
2020-02-17 12:53:47 +0700

    Update

--- App.tsx
+++ App.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import SplashScreen from './src/screens/SplashScreen';
 import HomeScreen from './src/screens/HomeScreen';
+import {SafeAreaView, StyleSheet} from 'react-native';
 
 export default function App() {
-  return <HomeScreen />;
+  return (
+    <SafeAreaView style={styles.container}>
+      <HomeScreen />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   // flex: 1,
-  //   // alignItems: 'center',
-  //   // justifyContent: 'center',
-  // },
+  container: {
+    flex: 1,
+  },
 });
