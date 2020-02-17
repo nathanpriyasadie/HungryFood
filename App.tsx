d91da191753fffb6cf829e9070e6890cf36ac381
KevinHartono35 <khartono77@yahoo.com>
2020-02-17 19:28:33 +0700

    Merge

--- App.tsx
+++ App.tsx
@@ -1,18 +1,6 @@
 import React from 'react';
-<<<<<<< HEAD
 import {Router} from './src/navigations';
 
 export default function App() {
   return <Router />;
-=======
-import {SafeAreaView} from 'react-native';
-import {RegisterScreen} from './src/screens';
-
-export default function App() {
-  return (
-    <SafeAreaView>
-      <RegisterScreen />
-    </SafeAreaView>
-  );
->>>>>>> upstream/develop
 }
